8f65657e53524cb245f007bef4a646f5987f0a66
mingjunior <chmingx@foxmail.com>
2019-05-24 12:59:02 +0800

    new tk_chat.py using tk.Notebook and OOP

--- tk_chat.py
+++ tk_chat.py
@@ -1,86 +1,102 @@
-import tkinter as tk 
+import tkinter as tk
+from tkinter import ttk 
 from tkinter import messagebox
-import random
+import time
 
-root = tk.Tk()
-root.title("PyTalk")
-root.iconbitmap('image/logo.ico')
-root.geometry("1000x800")
-# root.resizable(False, False)
+class App:
+	def __init__(self, master=None):
+		self.master = master
+		self.master.title('pytalk')
+		self.master.geometry('1000x800')
+		self.list_friends = [
+						    'Alan',
+						    'Will',
+						    'Bran',
+						    'Bob',
+						    'Kevin',
+						    'Daisy',
+						    'Mary',
+						    'Carl',
+						    'Lily',
+						    'Kate',
+						    'Jeo',
+						    '张三',
+						    '李四',
+						    '王五',
+						    '赵六',
+						    '陈七',
+						    '唐八',
+						    '沈九'
+						]
+		self.initWidgets()
 
-top_frm = tk.Frame(root, background='dodgerblue')
-top_frm.place(x=0, y=0, relwidth=1, relheight=0.1)
+	def initWidgets(self):
+		self.frm_btn = tk.Frame(self.master,bg='#eee')
+		self.frm_btn.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.05)
+		self.create_top_btn()
+		self.nb = ttk.Notebook(self.master)
+		self.nb.place(relx=0.1, rely=0.15, relwidth=0.8, relheight=0.7)
+		self.create_friends_tag()
+		self.text_input = tk.Entry(self.master,bg='#EEE', font=('宋体', 16, 'normal'))
+		self.text_input.place(relx=0.1, rely=0.9, relwidth=0.7, relheight=0.05)
+		self.btn_send_msg = tk.Button(self.master, 
+			text='发送消息', cursor='hand2', font=('Arial', 16, 'bold'), activeforeground='red', command=self.send)
+		self.btn_send_msg.place(relx=0.8, rely=0.9, relwidth=0.1, relheight=0.05)
+		
+	def create_friends_tag(self):
+		self.tab_font_style = ttk.Style().configure('.',font=('微软雅黑', 12))
+		self.list_nb_tab = []
+		# 循环创建标签
+		for frd in self.list_friends:
+			text_frd = tk.Text(self.nb, 
+								bg='#eee',
+								font=('微软雅黑', 16, 'normal'),
+								state='disable') # 将文本框设置成不可编辑状态
+			self.nb.add(text_frd, text=frd)
+			self.list_nb_tab.append(text_frd)
 
-# 左侧图标
-image_home = tk.PhotoImage(file='image/home.gif')
-top_label = tk.Button(top_frm,
-					image=image_home, 
-					font=('Arial', 20, 'bold'),
-					foreground='white',
-					background='dodgerblue',
-					compound=tk.LEFT) # 图片在文字左边
-top_label.bm = image_home # 手动为top_label添加属性bm，绑定图片变量，防止图片被回收
-top_label.place(relx=0.02, rely=0.5,anchor='w')
+	def create_top_btn(self):
+		self.btn_setting = tk.Button(self.frm_btn,
+									text='设置',
+									cursor='hand2',
+									width=10,
+									font=('Arial', 16, 'bold'),
+									activeforeground='red',
+									command=self.set)
+		self.btn_setting.pack(side='left', fill='y')
+		self.btn_add_friend = tk.Button(self.frm_btn,
+									text='添加好友',
+									cursor='hand2',
+									width=10,
+									font=('Arial', 16, 'bold'),
+									activeforeground='red',
+									command=self.add_friend)
+		self.btn_add_friend.pack(side='left', fill='y')
 
-# 主框架
-frm = tk.Frame(root, background='#EEE')
-frm.place(relx=0, rely=0.1, relwidth=1, relheight=0.9)
-# frm.pack(fill=tk.BOTH, expand=True)
-# 左右次框架
-frm_left = tk.Frame(frm, background='#EEE')
-frm_left.place(relx=0, rely=0, relwidth=0.2, relheight=1)
-# frm_left.pack(side='left', fill=tk.Y, expand=True)
-frm_right = tk.Frame(frm,background='lightgreen')
-frm_right.place(relx=0.2, rely=0, relwidth=0.8, relheight=1)
-# frm_right.pack(side='right', fill=tk.Y, expand=True)
+	def set(self):
+		pass
 
-# 左侧添加listbox，显示好友列表
-listbox_friends = tk.Listbox(frm_left)
-listbox_friends.pack(fill=tk.BOTH, expand=True)
+	def add_friend(self):
+		pass
 
-# 右侧分为聊天窗和文本输入窗
+	def send(self):
+		msg_string = self.text_input.get()
+		if not msg_string or msg_string == '\n':
+			messagebox.showwarning('warning', '消息为空,发送失败')
+		else:
+			msg_pre = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '\n'
+			# 选择当前的子窗口部件，即消息显示框
+			current_window = self.list_nb_tab[self.nb.index(self.nb.select())]
+			current_window.config(state='normal') # 修改文本框为可编辑状态
+			# 配置名为time_style的样式
+			current_window.tag_configure('time_style', font=('Arial', 10, 'normal'),foreground='green',justify='right')
+			current_window.tag_configure('text_style', font=('微软雅黑', 16, 'normal'),justify='right')
+			current_window.insert(tk.END, msg_pre, 'time_style')# 插入文本内容，设置使用time_style样式
+			current_window.insert(tk.END, msg_string + '\n', 'text_style')
+			current_window.config(state='disable')
+			self.text_input.delete(0, len(msg_string))
 
-# 左右两侧添加滑块
-# 创建Scrollbar组件，设置组件与左侧框的纵向滚动关联
-scroll_left = tk.Scrollbar(listbox_friends, command=listbox_friends.yview)
-scroll_left.pack(side='right',fill=tk.Y)
-# 设置左侧框的纵向滚动影响scroll_left滚动条
-listbox_friends.configure(yscrollcommand=scroll_left.set)
-
-
-# 左侧添加好友列表，从服务器获取
-list_friends = [
-	'Alan',
-	'Will',
-	'Bran',
-	'Bob',
-	'Kevin',
-	'Daisy',
-	'Mary',
-	'Carl',
-	'Lily',
-	'Kate',
-	'Jeo',
-	'张三',
-	'李四',
-	'王五'
-]
-list_friends.sort()
-# 循环创建好友标签按钮
-for i, name in enumerate(list_friends):
-	# 生成3个随机数，并将其格式化成16进制，转成颜色格式
-	color_number = [random.randrange(0, 256) for i in range(3)]
-	grayness = int(round(0.299*color_number[0]+0.587*color_number[1]+0.114*color_number[2]))
-	bg_color = "#%02x%02x%02x" % tuple(color_number)
-	fg_color = 'white' if grayness < 128 else 'black'
-
-	# # 创建并放置标签
-	# btn_friend = tk.Button(listbox_friends, text=name, fg=fg_color, bg=bg_color, font=('Arial', 20, 'bold'))
-	# btn_friend.place(relx=0, rely=0.11*i, relwidth=1, relheight=0.1)
-	# print(name)
-	label_friend = tk.Label(listbox_friends, text=name, fg=fg_color, bg=bg_color, font=('Arial', 20, 'bold'))
-	label_friend.pack()
-	listbox_friends.insert(tk.END, label_friend)
-
-
-root.mainloop()
+if __name__ == '__main__':
+	root = tk.Tk()
+	app = App(root)
+	root.mainloop()
